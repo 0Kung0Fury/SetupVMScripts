be29113ad6e9806198e337ddff5c03084ee021ab
0Kung0Fury <Kung_Fury00@proton.me>
2026-01-20 21:40:05 -0800

    Update installVPN.sh

--- installVPN.sh
+++ installVPN.sh
@@ -12,11 +12,17 @@ fi
 echo "Updating package lists..."
 apt update
 
+echo "installing vmwaretools on the VM..."
+apt-get install open-vm-tools open-vm-tooks-dekstop
+
 echo "Upgrading installed packages..."
 apt upgrade -y
 
 echo "Installing ProtonVPN CLI..."
-apt install -y proton-vpn-cli
+wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb   
+sudo dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo apt update
+sudo apt install proton-vpn-gnome-desktop proton-vpn-cli -y 
+sudo apt install libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
 
 echo "Removing unused packages..."
 apt autoremove -y
